781d4946d93433fd1ae63f127c2ccbff252ea81f
Sara Sofia Mora Trujillo <134168046+chofi-chan@users.noreply.github.com>
2023-12-14 16:22:04 -0500

    Verificación de tipos de datos y cálculo de la cantidad de hombres y mujeres fumadores.

--- analisis_de_datos.py
+++ analisis_de_datos.py
@@ -14,3 +14,12 @@ promedio_edad_no_fallecidos = personas_no_fallecidas["age"].mean()
 
 print(f"Promedio de Edad Fallecidos: {promedio_edad_fallecidos}")
 print(f"Promedio de Edad No Fallecidos: {promedio_edad_no_fallecidos}")
+
+tipos_de_datos = df.dtypes
+print("Tipos de Datos:")
+print(tipos_de_datos)
+
+fumadores_por_genero = df.groupby(['sex', 'smoking']).size().unstack()
+
+print("\nCantidad de Hombres y Mujeres Fumadores:")
+print(fumadores_por_genero)
